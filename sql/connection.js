52b11191c1ef7a858e0c7ba4554971f26de2287e
grantkee <grant.d.kee@gmail.com>
2020-04-04 17:37:20 -0500

    added env variables to connection.js. console logged to troubleshoot error.

--- sql/connection.js
+++ sql/connection.js
@@ -9,11 +9,15 @@ class Connection {
       this.pool = mysql.createPool({
         connectionLimit: 100,
         // host: 'remotemysql.com:3306',
-        host: 'https://37.59.55.185:3306',
+        host: process.env.SQL_HOST,
         user: process.env.SQL_USER,
         password: process.env.SQL_SECRET,
         database: process.env.SQL_DB
       })
+      console.log('host:', process.env.SQL_HOST);
+      console.log('user:', process.env.SQL_USER);
+      console.log('pword:', process.env.SQL_SECRET);
+      console.log('db:', process.env.SQL_DB);
       return this.pool
     }
     return this.pool
