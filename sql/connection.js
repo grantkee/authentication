7bc62000c37de675a32560f83842f7c5e474dcfc
grantkee <grant.d.kee@gmail.com>
2020-04-04 17:50:02 -0500

    corrected error by removing port from host and specifying manually. Port 3306 is default according to stackOverflow. Still specifying though.

--- sql/connection.js
+++ sql/connection.js
@@ -7,17 +7,19 @@ class Connection {
     if (!this.pool){
       console.log('creating mysql connection...')
       this.pool = mysql.createPool({
-        connectionLimit: 100,
+        connectionLimit: 1000,
         // host: 'remotemysql.com:3306',
         host: process.env.SQL_HOST,
         user: process.env.SQL_USER,
         password: process.env.SQL_SECRET,
-        database: process.env.SQL_DB
+        database: process.env.SQL_DB,
+        port: process.env.SQL_PORT
       })
       console.log('host:', process.env.SQL_HOST);
       console.log('user:', process.env.SQL_USER);
       console.log('pword:', process.env.SQL_SECRET);
       console.log('db:', process.env.SQL_DB);
+      console.log('port:', process.env.SQL_PORT);
       return this.pool
     }
     return this.pool
